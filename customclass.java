6713f7587badb3b9c6c4f73dffe6079b6c5d5663
giscardAsan <asantegiscard23@gmail.com>
2025-02-13 14:10:58 +0000

    commit

--- customclass.java
+++ customclass.java
@@ -1,7 +1,15 @@
+import java.util.ArrayList;
 import Myclasslist.*;
 
 public class customclass {
     public static void main(String agr[]) {
+
+        ArrayList list = new ArrayList<String>();
+        list.add("Giscard");
+        list.add("Jones");
+        list.add("Atta");
+
+        System.out.println(list.get(1));
 
         snakes obSnakes = new snakes();
         obSnakes.animal();
